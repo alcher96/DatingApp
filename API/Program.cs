7046ac8da2f076424efdf811df3738779a66067a
alcher96 <alcher96@mail.ru>
2024-09-29 23:26:39 +0300

    firss commmit

---
+++ API/Program.cs
@@ -0,0 +1,39 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddControllers();
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors(policy =>policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
+app.MapControllers();
+
+app.Run();
+
+
